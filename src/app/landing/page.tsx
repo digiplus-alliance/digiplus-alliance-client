538aa3faa48dce9401b6188c8c8e1bc02a109671
opeyemibioku <opeyemibioku@gmail.com>
2025-09-24 09:29:32 +0100

    testing

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 import LandingHero from "./widgets/landing-hero";
@@ -6,8 +6,9 @@ import DigiplusCanDoCards from "./widgets/digiplus-can-do-section";
 import WhyJoinDigiplus from "./widgets/why-join-digiplus";
 import Testimonials from "./widgets/testimonials";
 
+//landing page
 export default function Home() {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <div>
       <LandingHero />
