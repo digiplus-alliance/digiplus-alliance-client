55efbb36e4984aac07b9d41bbf62d44c07afa9b2
adesanyaolu <adenugaadesanyao@gmail.com>
2025-09-27 00:45:44 +0100

    feat: implement profile management with profile modal and update form

--- src/app/admin-dashboard/profile/page.tsx
+++ src/app/admin-dashboard/profile/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import ProfileModal from "./widgets/profile-page";
+import { useState } from "react";
+import UpdateProfileForm from "./widgets/update-profile-form";
+
+export const Profile = ({
+  toggleComponent,
+}: {
+  toggleComponent: (component: "profile" | "edit") => void;
+}) => {
+  return (
+    <div>
+      {/* Header */}
+      <div className="flex items-start md:items-center md:justify-between">
+        <div>
+          <h1 className="text-2xl text-primary font-bold">Profile</h1>
+        </div>
+        <div className="hidden md:flex md:items-center md:gap-4">
+          <Button onClick={() => toggleComponent("edit")}>Edit Profile</Button>
+        </div>
+      </div>
+      <div className="overflow-x-auto flex justify-center mt-10">
+        <ProfileModal />
+      </div>
+    </div>
+  );
+};
+
 export default function ProfilePage() {
-  return <div>Admin Profile Page</div>;
+  const [activeComponent, setActiveComponent] = useState<"profile" | "edit">(
+    "profile"
+  );
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "profile":
+        return <Profile toggleComponent={setActiveComponent} />;
+      case "edit":
+        return <UpdateProfileForm />;
+      default:
+        return <Profile toggleComponent={setActiveComponent} />;
+    }
+  };
+
+  return (
+    <div className="p-6 space-y-6 font-secondary md:bg-[#EBEBEB] rounded-tl-2xl h-full">
+      {renderComponent()}
+    </div>
+  );
 }
--- src/app/admin-dashboard/settings/page.tsx
+++ src/app/admin-dashboard/settings/page.tsx
@@ -1,3 +1,56 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import PasswordChange from "./widgets/reset-password";
+import { useState } from "react";
+
+export const Setting = ({
+  toggleComponent,
+}: {
+  toggleComponent: (component: "profile" | "edit") => void;
+}) => {
+  return (
+    <div className="p-6 font-secondary md:bg-[#EBEBEB] rounded-tl-2xl h-full">
+      {/* Header */}
+      <div className="flex items-start md:items-center md:justify-between">
+        <h1 className="text-2xl text-primary font-bold">Settings</h1>
+      </div>
+
+      <div
+        className="mt-10 p-6 flex items-center cursor-pointer"
+        onClick={() => toggleComponent("edit")}
+      >
+        <div className="">
+          <h3 className="text-lg font-semibold">Reset Password</h3>
+          <p className="text-sm text-gray-500">
+            Reset your password here and secure your account
+          </p>
+        </div>
+        <ArrowRight className="mt-4 text-gray-400 ml-10" />
+      </div>
+    </div>
+  );
+};
+
 export default function Settings() {
-  return <div className="p-4">Settings Page</div>;
+  const [activeComponent, setActiveComponent] = useState<"profile" | "edit">(
+    "profile"
+  );
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "profile":
+        return <Setting toggleComponent={setActiveComponent} />;
+      case "edit":
+        return <PasswordChange className="max-w-lg"/>;
+      default:
+        return <Setting toggleComponent={setActiveComponent} />;
+    }
+  };
+
+  return (
+    <div className="p-6 space-y-6 font-secondary md:bg-[#EBEBEB] rounded-tl-2xl h-full">
+      {renderComponent()}
+    </div>
+  );
 }
